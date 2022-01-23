17b61257b423388cd0a69064711b91e28fb92b55
Alexander Vidyakov <avidyakov@webim-team.ru>
2022-01-23 13:06:02 +0300

    Добавил swagger

--- project/config/settings.py
+++ project/config/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework_simplejwt',
+    'drf_yasg',
 
     'user_app.apps.UserAppConfig',
     'post_app.apps.PostAppConfig',
--- project/config/urls.py
+++ project/config/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+schema_view = get_schema_view(
+   openapi.Info(title='API', default_version='v1'),
+   public=True,
+   permission_classes=(permissions.AllowAny, )
+)
 
 urlpatterns = [
     path('post/', include('post_app.urls', namespace='post')),
@@ -10,4 +19,6 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('admin/', admin.site.urls),
+
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='docs'),
 ]
